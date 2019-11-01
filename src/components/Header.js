d797a4cf5e2309384e572784efa81752b5f6c134
NaluFigueira <anachfigueira@gmail.com>
2019-10-31 23:51:46 -0300

    Criando o Header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import '../assets/styles/Header.css';
+
+function Header(){
+  return(
+    <div className = "headerDiv">
+      <img className = "headerLogo" src = {require("../assets/images/facebook-1.png")} />
+      <div className = "headerProfile">
+        <span>Meu perfil</span>
+        <FontAwesomeIcon className="icon" icon = {faUser}/>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
